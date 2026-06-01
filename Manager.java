f9704e82260a652f52414e9dcd63d01b86eac981
Sandookie <132581131+Sandoksalamat@users.noreply.github.com>
2026-06-02 00:26:31 +0800

    Delete Manager.java

--- Manager.java
+++
@@ -1,5 +0,0 @@
-public class Manager extends Employee {
-    public Manager(int employeeID, String employeeName, String employeeRank) {
-        super(employeeID, employeeName, employeeRank);
-    }
-}
